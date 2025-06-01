6d1d83ef0935d6a92252b8f127d5ef0efc9813ed
Zihan Xu <zihanxu-amy@outlook.com>
2025-06-01 14:23:30 -0400

    makefile

--- README.md
+++ README.md
@@ -22,3 +22,9 @@ export PATH="$PREFIX/bin:$PATH"
 Multiboot Standard
 
 assemble boot.s with i686-elf-as boot.s -o boot.o
+
+i686-elf-gcc -c kernel.c -o kernel.o -std=gnu99 -ffreestanding -O2 -Wall -Wextra
+i686-elf-gcc -T linker.ld -o myos.bin -ffreestanding -O2 -nostdlib boot.o kernel.o -lgcc
+cp myos.bin isodir/boot/myos.bin
+grub-mkrescue -o myos.iso isodir
+qemu-system-i386 -cdrom myos.iso
--- barebonesOS/kernel.c
+++ barebonesOS/kernel.c
@@ -80,6 +80,12 @@ void terminal_setcolor(uint8_t color)
 	terminal_color = color;
 }
 
+/* terminal_newline - move current input position to next line*/
+void terminal_newline(){
+	terminal_row += 1;
+	terminal_column = 0;
+}
+
 void terminal_putentryat(char c, uint8_t color, size_t x, size_t y) 
 {
 	const size_t index = y * VGA_WIDTH + x;
@@ -88,12 +94,18 @@ void terminal_putentryat(char c, uint8_t color, size_t x, size_t y)
 
 void terminal_putchar(char c) 
 {
-	terminal_putentryat(c, terminal_color, terminal_column, terminal_row);
-	if (++terminal_column == VGA_WIDTH) {
-		terminal_column = 0;
-		if (++terminal_row == VGA_HEIGHT)
-			terminal_row = 0;
+	if (c == '\n'){
+		terminal_newline();
 	}
+	else{
+		terminal_putentryat(c, terminal_color, terminal_column, terminal_row);
+		if (++terminal_column == VGA_WIDTH) {
+			terminal_column = 0;
+			if (++terminal_row == VGA_HEIGHT)
+				terminal_row = 0;
+		}
+	}
+	
 }
 
 void terminal_write(const char* data, size_t size) 
@@ -113,5 +125,5 @@ void kernel_main(void)
 	terminal_initialize();
 
 	/* Newline support is left as an exercise. */
-	terminal_writestring("Hello, kernel World!\n");
+	terminal_writestring("Hello!\nkernel World!\n");
 }
